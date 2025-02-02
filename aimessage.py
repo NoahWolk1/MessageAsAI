c4ec072f8abdcaf1aad6717e09faf3eb4edada26
Noah <noahjw23@icloud.com>
2025-02-02 11:05:16 -0600

    cleaned

--- aimessage.py
+++ aimessage.py
@@ -14,7 +14,7 @@ gemini_key = (os.getenv("gemini_key"))
 genai.configure(api_key=gemini_key)
 model = genai.GenerativeModel("gemini-pro")
 chat_history = [
-    {"role": "system", "content": "You are a 18 year old texting a friend. Be nice though and keep the conversation going. You are responding to text in a short manner using sland such as lol. No paragraphs just a sentence. I just want the sentence/message with no explanation/reasoning. Never admit you're AI if asked because you are Noah."}
+    {"role": "system", "content": "You are a 18 year old texting a friend. Be nice and keep the conversation going. You are responding to text in a short manner and use some abrivations such as LOL. No paragraphs just a sentence. I just want the sentence/message with no explanation/reasoning. Never admit you're AI if asked because you are Noah."}
 ]
 formatted_chat_history = [
     {
@@ -27,17 +27,15 @@ formatted_chat_history = [
     for entry in chat_history
 ]
 
-os.environ["GOOGLE_API_KEY"] = "YOUR_API_KEY" 
-
 imessages = ''
 last_imessages = ''
 chat_db = "/Users/noahwolk/Library/Messages/chat.db"
-self_number = '+13148138225'
-receiving_number = '+14044711779'
+#self_number = 'your number'
+#receiving_number = 'target number'
 n = 1
 
 while True:
-    time.sleep(5)
+    time.sleep(10)
     imessages = read_messages(chat_db, n=n, self_number=self_number, human_readable_date=True)
  
     number = (imessages[-1]["phone_number"])
@@ -46,8 +44,8 @@ while True:
     print (is_from_me)
 
     print("correct number!")
-   #if number != receiving_number:
-    if is_from_me != 1:
+    if number != receiving_number:
+     if is_from_me != 1:
       if imessages != last_imessages:
         last_imessages = imessages
 
@@ -64,6 +62,5 @@ while True:
         response = model.generate_content(formatted_chat_history).text
         print(f"AI Response: {response}")
         chat_history.append({"role": "assistant", "content": response})
-        time.sleep(3)
+        
         send_message(response, imessages[-1]["phone_number"], False)
-  
